ca95ac39250df7070f47afe33e726fd0b40856ba
Aleix Casals <alcaac1617daw2@gmail.com>
2017-03-13 20:47:55 +0100

    “cookies3 commit 1 solucionat

--- index.php
+++ index.php
@@ -5,19 +5,19 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF­8">
-        <title>Desplegant una aplicació PHP sobre Heroku</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>Desplegant una aplicació PHP sobre Heroku</title>
     </head>
     <body>
-<?php 
-    echo "Afegint Cookies<br>";
-    echo "Sóc el desenvolupador alcaac1617daw2";
-    setcookie("nom","Aleix Casals",time()+3600,"/","",0); 
-    setcookie("edat","20",time()+3600,"/","",0);
-    setcookie("cicle","DAW",time()+3600,"/","",0);
-    setcookie("curs","2n",time()+3600,"/","",0);
-    setcookie("any","2016­2017",time()+3600,"/","",0);            
-?>  
-    </body>    
-</html> 
+        <?php
+            echo "Afegint Cookies<br>";
+            echo "Sóc el desenvolupador alcaac1617daw2<br>";
+            setcookie("name", "Aleix Casals", time()+3600, "/", "", 0);               
+            setcookie("edat", "20", time()+3600, "/", "", 0);
+            setcookie("cicle", "daw", time()+3600, "/", "", 0);
+            setcookie("curs", "2n", time()+3600, "/", "", 0);
+            setcookie("any", "2016-17", time()+3600, "/", "", 0);
+        ?>
+    </body>
+</html>
